b4c29ff9e06d69af98ae24b14bdfe720f87816f0
Georges Chouchani <gachouchani@ndu.edu.lb>
2021-10-24 00:45:07 +0300

    fixing frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,65 @@
 <template>
+
   <div>
+   <div>
+  
+  <b-navbar variant="faded" type="light" style="background-color: #0000FF">
+    <b-navbar-brand href="/">
+      <img src="../static/cosmostarter.png" class="d-inline-block align-top" alt="Cosmostarter Airdrop" width="50" height="50">
+      Cosmostarter Airdrop
+    </b-navbar-brand>
+  </b-navbar>
+</div>
+<b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
   <b-form inline>
     <label class="sr-only" for="inline-form-input-name">Name</label>
     <b-form-input
@@ -20,30 +80,40 @@
 </template>
 
 <script>
-function EnableTxt() {
-       document.getElementById("inline-form-input-username").removeAttribute("disabled");
-   }
-
-window.onload = async () => {
+export default {
+  data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    mounted () {
+    window.onload = async () => {
     if (!window.keplr) {
         alert("Please install keplr extension");
     } else {
-        EnableTxt()
         const chainId = "cosmoshub-4";
 
         // Enabling before using the Keplr is recommended.
         // This method will ask the user whether or not to allow access if they haven't visited this website.
         // Also, it will request user to unlock the wallet if the wallet is locked.
         await window.keplr.enable(chainId);
+        document.getElementById("inline-form-input-username").disabled = false;
     
-        
-    
-        
         
     }
 }
 
-export default {
+  },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      }
+    },
   
 }
+  
 </script>
